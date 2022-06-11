3a2c97a246d3564974792ea4e1e816349ae38872
Nadav Galili <67638018+nadav-galili@users.noreply.github.com>
2022-06-11 20:37:24 +0300

    apptext

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
-import { StyleSheet, StatusBar, Platform } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { StyleSheet, StatusBar, Platform, View, Text } from "react-native";
+import AppText from "./app/components/AppText";
+// import WelcomeScreen from "./app/screens/WelcomeScreen";
+// import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppText>
+        I love react app this is my first react native line heres some more text
+      </AppText>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "orange",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "orange",
+//     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+//     // alignItems: "center",
+//     // justifyContent: "center",
+//   },
+// });
